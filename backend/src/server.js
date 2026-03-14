8567e055d5312189c5f7daa0a72fe0d5002def8d
Ragha1386 <raghav.anand1386@gmail.com>
2026-03-15 02:25:15 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,22 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import authRoutes from './routes/auth.routes.js'
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+import path from 'path'
 dotenv.config()
 const app=express()
-app.use(express.json())
-process.env.PORT
-app.listen(process.env.PORT || 3000,()=>{console.log("Server is running on port " + (process.env.PORT || 3000))})
+const __dirname=path.resolve()
+
 app.use('/api/auth',authRoutes)
+app.use('/api/messages',messageRoutes)
+
+//make ready for deployment 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
+app.listen(process.env.PORT || 5000,()=>{console.log("Server is running on port " + (process.env.PORT || "failed to start"))})
+
+ 
